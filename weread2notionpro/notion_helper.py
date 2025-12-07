e327df0736ab43dc95468e042c8bbc40395fd094
tingting0414a <tingting0414a@163.com>
2025-12-07 18:19:59 +0800

    Update notion_helper.py

--- weread2notionpro/notion_helper.py
+++ weread2notionpro/notion_helper.py
@@ -429,7 +429,7 @@ class NotionHelper:
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def query(self, **kwargs):
         kwargs = {k: v for k, v in kwargs.items() if v}
-        return self.client.databases.query(**kwargs)
+        eturn self.client.databases.retrieve(**kwargs)
 
     @retry(stop_max_attempt_number=3, wait_fixed=5000)
     def get_block_children(self, id):
